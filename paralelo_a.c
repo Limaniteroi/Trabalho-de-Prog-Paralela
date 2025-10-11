fa586dd594f3c21d1c3e78be141b2ce0d477c5a4
RodCrvlh <digaorrvcc@gmail.com>
2025-10-11 18:33:12 -0300

    mudanca de comentario e tamanho

--- paralelo_a.c
+++ paralelo_a.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "mpi.h"
-#define TAM 1000
+#define TAM 1000000
 
 int main(int argc, char** argv){
 	int meu_ranque, num_procs, etiq = 0, i=0;
@@ -87,6 +87,6 @@ int main(int argc, char** argv){
     }
 
 	
-	// Limpa as pendências deixadas pelo MPI
+	// Limpa as pendencias deixadas pelo MPI
 	MPI_Finalize();
 }
