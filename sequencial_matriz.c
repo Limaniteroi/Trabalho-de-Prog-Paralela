2d5097df068aca56f2f59e1968aac81ea8d661e0
Marianna Lima <140180165+Limaniteroi@users.noreply.github.com>
2025-11-26 20:16:19 -0300

    Adicionando código sequencial e estático

---
+++ sequencial_matriz.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <time.h>
+
+int matriz_1[1000][1000];
+int matriz_2[1000][1000];
+int matriz_resultado[1000][1000];
+
+int main(void){
+
+    int i, j, k;
+    clock_t tempo_inicial, tempo_final;
+    double tempo_decorrido;
+
+    for(i = 0; i < 1000; i++) {
+        for(j = 0; j < 1000; j++){
+
+            matriz_1[i][j] = 1;
+            matriz_2[i][j] = 1;
+        }
+    }
+
+    tempo_inicial = clock();
+
+    for(i = 0; i < 1000; i++) {
+        for(j = 0; j < 1000; j++){
+
+            int soma_produto = 0; 
+
+            for(k = 0; k < 1000; k++){ 
+                soma_produto += matriz_1[i][k] * matriz_2[k][j];
+            }
+            
+            matriz_resultado[i][j] = soma_produto;
+            
+        }
+    }
+
+    tempo_final = clock();
+
+    tempo_decorrido = (double)(tempo_final - tempo_inicial)/ CLOCKS_PER_SEC;
+
+    printf("Tempo de execução: %f segundos\n", tempo_decorrido);
+
+    return 0;
+}
